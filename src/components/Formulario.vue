c1f6ca2a7de0d930946b8c65c0deb120d4d52a05
Sarah781 <sarah7bvs@gmail.com>
2023-03-08 17:45:17 -0300

    projeto vue

---
+++ src/components/Formulario.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>
+                            {{ tempoFormatado }}
+                        </strong>
+                    </section>
+                    <button class="button" @click="iniciarTarefa">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="pararTarefa">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>   
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'FormulárioUsuario',
+    data () {
+        return {
+            tarefa: '',
+            tempo: 0,
+            cronometro: 0,
+        }
+    },
+    computed: {
+        tempoFormatado() : string {
+            return new Date(this.tempo * 1000).toISOString().substr(11, 8)
+        }
+    },
+    methods: {
+        iniciarTarefa() {
+            //começar a contar o tempo
+            this.cronometro = setInterval(() => {
+                this.tempo+=1
+            }, 1000)
+        },
+        pararTarefa() {
+            //parar de contar o tempo
+            clearInterval(this.cronometro)
+        }
+    },
+})
+</script>
